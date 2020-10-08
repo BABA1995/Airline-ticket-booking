9cd1ad277da5b6f7451dfbe34bd184d8ae1491cc
BABA 1995 <fakruddinbaba16@gmail.com>
2020-10-09 02:35:21 +0530

    booking-form completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookingFormComponent } from './booking-form/booking-form.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+// import { MatMomentDateModule } from '@angular/material-moment-adapter/core';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +29,14 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    CommonModule,
+    MatDividerModule,
+    MatDatepickerModule,MatNativeDateModule,
+    // MatMomentDateModule
+    MatButtonModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/booking-form/booking-form.component.ts
+++ src/app/booking-form/booking-form.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Travel{
+  viewValue: string;
+  number: string;
+  scheduledTime: string;
+}
+
 @Component({
   selector: 'app-booking-form',
   templateUrl: './booking-form.component.html',
   styleUrls: ['./booking-form.component.css']
 })
 export class BookingFormComponent implements OnInit {
+  viewValue: String;
+  scheduledTime: String;
+traveltype: Travel[] =  [
+  {viewValue:'Arrival'},
+  {viewValue:'Departure'}
+];
+flightnumber: Travel[] = [
+
+  {  number:'302'},
+  {  number:'303'},
+  {  number:'304'},
+
+];
+fighttimings: Travel[] =[
+{  scheduledTime:'2017-12-11T01:06:00.000'},
+{  scheduledTime:'2017-12-11T01:10:00.000'},
+{  scheduledTime:'2017-12-11T01:15:00.000'},
+]
+
 
   constructor() { }
 
